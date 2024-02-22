3e03f7571ffaa5b0f9f8ebefb401bf2d2d388536
Jose Becerra <josebecerra18@Joses-Mac-mini.local>
2024-02-21 19:44:46 -0700

    Starting point

--- screens/RootLayout.tsx
+++ screens/RootLayout.tsx
@@ -1,8 +1,6 @@
 
 import { Outlet } from "react-router-dom";
 import Topbar from "../src/components/Topbar/Topbar";
-import Bottombar from "../src/components/Bottombar";
-import LeftSidebar from "../src/components/LeftSidebar";
 
 const RootLayout = () => {
   return (
