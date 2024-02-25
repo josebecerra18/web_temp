47b70d7afd1c04df33c449f9a40105b9b30d2f5a
Jose Becerra <josebecerra18@Joses-Mac-mini.local>
2024-02-24 17:37:41 -0700

    Clear data

--- screens/RootLayout.tsx
+++ screens/RootLayout.tsx
@@ -1,28 +1,9 @@
-
-//Topbar and CategoryTopbar layouts that wont move while outlet changes
-import { Outlet } from "react-router-dom";
-import Topbar from "../src/components/Topbar/Topbar";
-import CategoryTopbar from "@/components/CategoryTopbar";
+import React from 'react'
 
 const RootLayout = () => {
   return (
-     <div className="w-full md:flex">
-      <Topbar />
-      <div style={{marginTop:80,justifyContent: 'center', width: '100%', }}>
-      <CategoryTopbar/>
-      </div>
-      <div className="flex flex-row bg-neutral-100 h-screen w-screen overflow-hidden">
-        <div className=" bg-yellow-500 w-60 p-3 flex flex-col h-screen"   >
-          <h1 className="text-amber-300">Left3</h1> 
-        </div>
-        <div><Outlet /></div>
-        <div className=" bg-blue-600 w-60 p-3 flex flex-col h-screen"   >
-          <h1 className="text-amber-300">Right</h1> 
-        </div>
-      </div>
-
-    </div>
+    <div>RootLayout</div>
   )
 }
 
-export default RootLayout;
+export default RootLayout
--- src/commons/CategoriesButtons.tsx
+++ src/commons/CategoriesButtons.tsx
@@ -1,19 +1,9 @@
-import {allCategoriesObj} from '../components/ListaDeCategorias';
+import React from 'react'
 
 const CategoriesButtons = () => {
   return (
-    <div className='bg-gray-100 flex  justify-center align-middle pt-4 pb-4'>
-        {allCategoriesObj.map(val => (
-            <button className='pl-3 pr-3 pt-2 pb-2 ml-3 border bg-white
-             rounded-full border-purple-200 text-purple-600 text-sm font-semibold hover:text-white hover:bg-purple-600 hover:border-transparent
-             focus:outline-none focus:bg-red focus:text-white focus:ring-2 focus:ring-purple-600 focus:ring-offset-2 ' style={{whiteSpace: 'nowrap'}}>
-                {val.categoryNameProfileEN}
-            </button>
-        ))}
-    </div>
+    <div>CategoriesButtons</div>
   )
 }
 
 export default CategoriesButtons
-
- 
